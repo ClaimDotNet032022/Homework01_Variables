1465f83c08cc692778fde7233325c560d7c31b4a
Eric Ray <erichamion@gmail.com>
2022-04-26 17:31:35 -0500

    Almost right

--- VariablesHomework/VariablesHomework.cs
+++ VariablesHomework/VariablesHomework.cs
@@ -8,6 +8,12 @@ namespace VariablesHomework
         [TestMethod]
         public void Declaration()
         {
+            int number = 25;
+            bool boolean = true;
+            double decimalValue = 99.9;
+            float floatingDecimalValue = 25.4f;
+            string words = "I am a programer now";
+            char character = 'z';
             Assert.IsTrue(number == 25); // Make this pass without changing this statement (add other statements before or after to make it pass)
             Assert.IsTrue(boolean == true); // Make this pass without changing this statement
             Assert.IsTrue(decimalValue == 99.9); // Make this pass without changing this statement
@@ -19,39 +25,45 @@ namespace VariablesHomework
         [TestMethod]
         public void Assignment()
         {
-            int i = 0;
+            int i = 25;
             Assert.IsTrue(i == 25); // Make this pass without changing this statement (add or modify a statement above to make it pass)
 
+            i = 100;
             Assert.IsTrue(i == 100); // Then make this pass without changing this statement
 
-            float f = 0.0f;
-            Assert.IsTrue(f == 3.5); // Make this pass without changing this statement (except you might need to add something)
+            float f = 3.5f;
+            Assert.IsTrue(f == 3.5f); // Make this pass without changing this statement (except you might need to add something)
 
-            Assert.IsTrue(f == 99.9); // Then make this pass without changing this statement (except you might need to add something)
+            f = 99.9f;
+            Assert.IsTrue(f == 99.9f); // Then make this pass without changing this statement (except you might need to add something)
 
-            bool b = false;
+            bool b = true;
             Assert.IsTrue(b); // Make this pass without changing this statement
 
+            b = false;
             Assert.IsTrue(b == false); // Then make this pass without changing this statement
 
-            double d = 0.0;
+            double d = 10.0;
             Assert.IsTrue(d == 10.0); // Make this pass without changing this statement
 
+            d = 20.0;
             Assert.IsTrue(d == 20.0); // Then make this pass without changing this statement
 
 
-            string s = "";
+            string s = "word";
             Assert.IsTrue(s == "word"); // Make this pass without changing this statement
 
+            s = "next";
             Assert.IsTrue(s == "next"); // Then make this pass without changing this statement
 
-            char c = 'a';
+            char c = 'b';
             Assert.IsTrue(c == 'b'); // Make this pass without changing this statement
 
+            c = 'c';
             Assert.IsTrue(c == 'c'); // Then make this pass without changing this statement
 
             int z = i;
-            Assert.IsTrue(z == /*Answer here*/); // Make this pass by ONLY filling in the answer
+            Assert.IsTrue(z == 100); // Make this pass by ONLY filling in the answer
         }
 
         // Refer to this if you need help https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/operators/arithmetic-operators
@@ -59,74 +71,74 @@ namespace VariablesHomework
         public void Operators()
         {
             int a = 0;
-            int b = a + 13; 
-            Assert.IsTrue(b == /*Answer here*/); // Make this pass by ONLY filling in the answer
+            int b = a + 13;
+            Assert.IsTrue(b == 13); // Make this pass by ONLY filling in the answer
 
             float f = 12.3f;
             float c = f - 7.43f;
-            Assert.IsTrue(c == /*Answer here*/); // Make this pass by ONLY filling in the answer
-            Assert.IsTrue(f == /*Answer here*/); // Make this pass by ONLY filling in the answer
+            Assert.IsTrue(c == 12.3f - 7.43f); // Make this pass by ONLY filling in the answer
+            Assert.IsTrue(f == 12.3f); // Make this pass by ONLY filling in the answer
 
             bool d = false;
             bool e = !d;
-            Assert.IsTrue(e == /*Answer here*/); // Make this pass by ONLY filling in the answer
+            Assert.IsTrue(e == true); // Make this pass by ONLY filling in the answer
 
             int anotherInt = 12;
-            int g = -anotherInt; 
-            Assert.IsTrue(g == /*Answer here*/); // Make this pass by ONLY filling in the answer
+            int g = -anotherInt;
+            Assert.IsTrue(g == -12); // Make this pass by ONLY filling in the answer
 
             int yetAnotherInt = 23456;
             int t = yetAnotherInt * 3;
-            Assert.IsTrue(t == /*Answer here*/); // Make this pass by ONLY filling in the answer
+            Assert.IsTrue(t == 70368); // Make this pass by ONLY filling in the answer
 
             int z = 23456;
             z *= 3;
-            Assert.IsTrue(z == /*Answer here*/); // Make this pass by ONLY filling in the answer
+            Assert.IsTrue(z == 70368); // Make this pass by ONLY filling in the answer
 
             int h = 23;
             h++;
-            Assert.IsTrue(h == /*Answer here*/); // Make this pass by ONLY filling in the answer
+            Assert.IsTrue(h == 24); // Make this pass by ONLY filling in the answer
             h--;
-            Assert.IsTrue(h == /*Answer here*/); // Make this pass by ONLY filling in the answer
+            Assert.IsTrue(h == 23); // Make this pass by ONLY filling in the answer
             ++h;
-            Assert.IsTrue(h == /*Answer here*/); // Make this pass by ONLY filling in the answer
+            Assert.IsTrue(h == 24); // Make this pass by ONLY filling in the answer
             --h;
-            Assert.IsTrue(h == /*Answer here*/); // Make this pass by ONLY filling in the answer
+            Assert.IsTrue(h == 23); // Make this pass by ONLY filling in the answer
             h += 1;
-            Assert.IsTrue(h == /*Answer here*/); // Make this pass by ONLY filling in the answer
+            Assert.IsTrue(h == 24); // Make this pass by ONLY filling in the answer
             h -= 1;
-            Assert.IsTrue(h == /*Answer here*/); // Make this pass by ONLY filling in the answer
+            Assert.IsTrue(h == 23); // Make this pass by ONLY filling in the answer
 
             string l = "Hello! ";
             string m = l + "You!";
-            Assert.IsTrue(m == /*Answer here*/); // Make this pass by ONLY filling in the answer
+            Assert.IsTrue(m == "Hello! You!"); // Make this pass by ONLY filling in the answer
 
             int v = 5;
             int x = v % 4;
-            Assert.IsTrue(x == /*Answer here*/); // Make this pass by ONLY filling in the answer
+            Assert.IsTrue(x == 1); // Make this pass by ONLY filling in the answer
 
             int finalInt = 15;
             int divisionResult = 15 / 3;
-            Assert.IsTrue(divisionResult == /*Answer here*/); // Make this pass by ONLY filling in the answer
+            Assert.IsTrue(divisionResult == 5); // Make this pass by ONLY filling in the answer
 
             finalInt = 15;
             finalInt /= 3;
-            Assert.IsTrue(finalInt == /*Answer here*/); // Make this pass by ONLY filling in the answer
+            Assert.IsTrue(finalInt == 5); // Make this pass by ONLY filling in the answer
 
             int orderOfOperations = 2;
             orderOfOperations = 2 + 12 * (orderOfOperations * 3);
-            Assert.IsTrue(orderOfOperations == /*Answer here*/); // Make this pass by ONLY filling in the answer
+            Assert.IsTrue(orderOfOperations == 74); // Make this pass by ONLY filling in the answer
         }
 
         [TestMethod]
         public void Ranges()
         {
-            Assert.IsTrue(int.MaxValue == /*Answer here*/); // Make this pass
-            Assert.IsTrue(int.MinValue == /*Answer here*/); // Make this pass
-            Assert.IsTrue(float.MaxValue == /*Answer here*/); // Make this pass
-            Assert.IsTrue(float.MinValue == /*Answer here*/); // Make this pass
-            Assert.IsTrue(double.MaxValue == /*Answer here*/); // Make this pass
-            Assert.IsTrue(double.MinValue == /*Answer here*/); // Make this pass
+            Assert.IsTrue(int.MaxValue == 2_147_483_647); // Make this pass
+            Assert.IsTrue(int.MinValue == -2_147_483_648); // Make this pass
+            Assert.IsTrue(float.MaxValue == 3.40282347e38f); // Make this pass
+            Assert.IsTrue(float.MinValue == -3.40282347e38f); // Make this pass
+            Assert.IsTrue(double.MaxValue == 1.7976931348623157e308); // Make this pass
+            Assert.IsTrue(double.MinValue == -1.7976931348623157e308); // Make this pass
         }
 
         [TestMethod]
@@ -134,12 +146,12 @@ namespace VariablesHomework
         {
             unchecked // ignore this, it simple means im telling the compiler i know what I am doing.
             {
-                Assert.IsTrue(int.MaxValue + 1 == /*Answer here*/); // Make this pass (you can use a literal number or a symbol)
-                Assert.IsTrue(int.MinValue - 1 == /*Answer here*/); // Make this pass (you can use a literal number or a symbol)
-                Assert.IsTrue(float.MaxValue + 1 == /*Answer here*/); // Make this pass (you can use a literal number or a symbol)
-                Assert.IsTrue(float.MinValue - 1 == /*Answer here*/); // Make this pass (you can use a literal number or a symbol)
-                Assert.IsTrue(double.MaxValue + 1 == /*Answer here*/); // Make this pass (you can use a literal number or a symbol)
-                Assert.IsTrue(double.MinValue - 1 == /*Answer here*/); // Make this pass (you can use a literal number or a symbol)
+                Assert.IsTrue(int.MaxValue + 1 == int.MinValue); // Make this pass (you can use a literal number or a symbol)
+                Assert.IsTrue(int.MinValue - 1 == int.MaxValue); // Make this pass (you can use a literal number or a symbol)
+                Assert.IsTrue(float.MaxValue + 1 == float.MaxValue); // Make this pass (you can use a literal number or a symbol)
+                Assert.IsTrue(float.MinValue - 1 == float.MinValue); // Make this pass (you can use a literal number or a symbol)
+                Assert.IsTrue(double.MaxValue + 1 == double.MaxValue); // Make this pass (you can use a literal number or a symbol)
+                Assert.IsTrue(double.MinValue - 1 == double.MinValue); // Make this pass (you can use a literal number or a symbol)
             }
         }
 
@@ -148,11 +160,11 @@ namespace VariablesHomework
         {
             int i = 13;
             double d = i;
-            Assert.IsTrue(d == /*Answer here*/); // Make this pass by ONLY filling in the answer
+            Assert.IsTrue(d == 13.0); // Make this pass by ONLY filling in the answer
 
             // Why does that compile andwork but this doesnt? (Add a comment with your answer)
-            // double d = 15.5;
-            // int c = d;
+            //d = 15.5;
+            //int c = d;  // There is no implicit cast from double to int because it is a narrowing/lossy conversion
         }
 
         [TestMethod]
@@ -160,11 +172,11 @@ namespace VariablesHomework
         {
             // what am i doing here that make the above work? (Add a comment with your answer)
             double d = 15.5;
-            int c = (int)d;
-            Assert.IsTrue(c == /*Answer here*/); // Make this pass by ONLY filling in the answer
+            int c = (int)d; // Explicit cast
+            Assert.IsTrue(c == 15); // Make this pass by ONLY filling in the answer
 
             string e = d.ToString();
-            Assert.IsTrue(e == /*Answer here*/); // Make this pass by ONLY filling in the answer
+            Assert.IsTrue(e == "15.5"); // Make this pass by ONLY filling in the answer
         }
 
         [TestMethod]
@@ -172,7 +184,7 @@ namespace VariablesHomework
         {
             string a = "67";
             int b = int.Parse(a);
-            Assert.IsTrue(b == /*Answer here*/); // Make this pass by ONLY filling in the answer
+            Assert.IsTrue(b == 67); // Make this pass by ONLY filling in the answer
         }
     }
 }
